4a3fa62e6bfa81e8e3ce3b4aca028d8be4477588
nyvnge <sandersonnyange@gmail.com>
2023-03-24 23:35:03 +0300

    Added main js file and few changes and implementations

---
+++ main.js
@@ -0,0 +1,48 @@
+var stopwatch = document.getElementById("stopwatch");
+var startBtn = document.getElementById("start-btn");
+var timeoutId = null;
+var ms = 0;
+var sec = 0;
+var min = 0;
+
+// Function to start stopwatch
+
+function start(flag){
+    if (flag){
+        startBtn = true;
+    }
+    timeoutId = setTimeout(function(){
+        ms = parseInt(ms);
+        sec = parseInt(sec);
+        min = parseInt(min);
+
+        ms++;
+
+        if (ms == 100){
+            sec = sec + 1;
+            ms = 0;
+        }
+        if (sec == 60){
+            min == min + 1;
+            sec = 0;
+        }
+        if (ms < 10){
+            ms = '0' + ms;
+        }
+        if (sec < 10){
+            sec = '0' + sec;
+        }
+        if (min < 10){
+            min = '0' + min;
+        }
+
+        stopwatch.innerHTML = min + ':' + sec + ':' + ms;
+        start();
+    }, 10); // delay time 10 ms
+}
+
+// Function to pause stopwatch
+function pause(){
+    clearTimeout(timeoutId);
+    startBtn.disabled = false;
+}
